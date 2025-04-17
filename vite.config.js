fcb1452ed8d3b282177ddf877e63e9d752e32b5b
stanleynephi <nul23001@byui.edu>
2025-04-17 08:48:28 +0000

    ready for build

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        about: resolve(__dirname, "src/about/index.html"),
+        about: resolve(__dirname, "src/aboutus/index.html"),
         profile: resolve(__dirname, "src/profile/index.html"),
         contact: resolve(__dirname, "src/contact/index.html"),
-        jobs: resolve(__dirname, "src/jobs/index.html"),
+        jobs: resolve(__dirname, "src/job/index.html"),
       },
     },
   },
